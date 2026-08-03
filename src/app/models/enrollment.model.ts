0df9f0b41bdfc5964bae61424fba6ace79b18df7
kalu <kaluabiyu@gmail.com>
2026-08-04 00:09:02 +0300

    Centralized State with SignalStore

--- src/app/models/enrollment.model.ts
+++ src/app/models/enrollment.model.ts
@@ -9,3 +9,20 @@ export interface EnrollmentResult {
   enrolledAt: string;
 }
 
+export interface Enrollment {
+id: string;
+studentId: number;
+studentName: string;
+courseId: number;
+courseName: string;
+status: 'Pending' | 'Approved' | 'Rejected';
+enrolledAt: string;
+}
+
+export interface EnrollmentSummaryDto {
+  id: number;
+  studentName: string;
+  courseName: string;
+  status: 'Pending' | 'Approved' | 'Rejected';
+  enrolledAt: string;
+}
