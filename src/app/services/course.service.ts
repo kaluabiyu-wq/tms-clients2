2205e6966b90504f1f118b9d4c4fe5a1313029ba
kalu <kaluabiyu@gmail.com>
2026-08-11 16:00:41 +0300

    Breaking the CORS Lock

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,31 +1,32 @@
-import { inject, Injectable } from "@angular/core";
+import { inject, Injectable, Service } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { Course, CourseDetail, PagedResponse } from "../models/course.model";
 import { CreateEnrollmentRequest,EnrollmentResult } from "../models/enrollment.model";
+import { environment } from "../../environments/environment.development";
 
-@Injectable({ providedIn: "root" })
+@Service()
 export class CourseService {
   private http = inject(HttpClient);
-  private baseUrl = "http://localhost:5285/api/v1/courses";
+  private  readonly base = `${environment.apiUrl}/courses`;
 
   
   getAll(page = 1, pageSize = 50) {
     return this.http
-      .get<PagedResponse<Course>>(this.baseUrl, {
-        params: { page: page.toString(), pageSize: pageSize.toString() },
+      .get<PagedResponse<Course>>(this.base, {
+        params: { page: '1', pageSize: '50' },
       })
-      .pipe(map((p) => p.items));
+      .pipe(map(response => response.items));
   }
 
 
   getById(id: string) {
-    return this.http.get<CourseDetail>(`${this.baseUrl}/${id}`);
+    return this.http.get<CourseDetail>(`${this.base}/${id}`);
   }
 
 
   enroll(courseId: number, request: CreateEnrollmentRequest) {
-    return this.http.post<EnrollmentResult>( `${this.baseUrl}/${courseId}/enrollments`,
+    return this.http.post<EnrollmentResult>( `${this.base}/${courseId}/enrollments`,
       request,
     );
   }
