a76235bca1f2ff7e50c24a34f645d2edaa3366de
kalu <kaluabiyu@gmail.com>
2026-08-30 22:35:48 +0300

    test Playwright E2E Suite

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,6 +27,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
+ 
+    baseURL: "http://localhost:4200",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -49,8 +51,7 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
     { name: "setup",
-      testDir: "./e2e",
-      testMatch: /.*\.setup\.ts/
+        testMatch: /.*\.setup\.ts/,
     },
     {
       name: "tests",
